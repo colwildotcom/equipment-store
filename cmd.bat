903acdb8bf8d6e23dcd6348ac86a35078dbd99ce
Colin Williams <col@colwil.com>
2020-11-09 09:37:28 +0000

    first message

---
+++ cmd.bat
@@ -0,0 +1,10 @@
+cd /D "%~dp0"
+
+rem set HTTP_PROXY=http://cowi01:Londontown%40002@devproxy.shbmain.shb.biz:8088
+rem set HTTPS_PROXY=%HTTP_PROXY%
+rem set FTP_PROXY=%HTTP_PROXY%
+
+set http_proxy=http://192.168.56.1:3128
+set https_proxy=http://192.168.56.1:3128
+
+cmd.exe
